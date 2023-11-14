fd12fb1de8bbeea21aa19760d28d50c1c12d659d
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-11-14 11:09:44 +0100

    add associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :tags, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 60 }
   validates :content, presence: true, length: { minimum: 1500 }
