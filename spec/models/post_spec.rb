7ff685f88a4a954069683beb04c7b43742dcdeec
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-11-14 11:01:23 +0100

    add text to check validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) do
+    User.create(
+      first_name: 'John',
+      last_name: 'Smith',
+      username: 'username',
+      email: 'test@test.com',
+      password: 'password'
+    )
+  end
+  let(:category) do
+    Category.create(category_name: 'test')
+  end
+  let(:post) do
+    Post.new(
+      title: Faker::Lorem.characters(number: 50),
+      content: Faker::Lorem.characters(number: 1550),
+      user_id: user.id,
+      category_id: category.id
+    )
+  end
+  it 'Title should not be blank' do
+    post.title = ''
+    expect(post).to_not be_valid
+  end
+  it 'Title should not be over 60 characters' do
+    post.title = Faker::Lorem.characters(number: 62)
+    expect(post).to_not be_valid
+  end
+  it 'Content should not be blank' do
+    post.content = ''
+    expect(post).to_not be_valid
+  end
+  it 'Content should not be less than 1500 characters' do
+    post.content = Faker::Lorem.characters(number: 1000)
+    expect(post).to_not be_valid
+  end
 end
