d96b3063cac1b3d1b9dd76f1ef7a21a308156b48
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-11-14 08:53:25 +0100

    add associations to posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :username, presence: true
   validates :username, uniqueness: true
+
+  has_many :posts, depenent: :destroy
 end
