891988309921a6ac824ecc41720c42057eb61496
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-11-16 09:34:40 +0100

    changed syntax for regex

--- app/models/social.rb
+++ app/models/social.rb
@@ -2,7 +2,7 @@ class Social < ApplicationRecord
   belongs_to :user
 
   validates :link, presence: true, format: {
-    with: /\A((http|https):\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix,
+    with: %r{\A((http|https)://)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}ix,
     message: 'Invalid URL'
   }
 end
