c4d8a0cc82d4155fc5fcaa837cc295c8129858ad
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-11-16 12:57:39 +0100

    generate first request test for post

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    user = FactoryBot.create(:user, username: Faker::Internet.username)
+    category = FactoryBot.create(:category)
+    post = FactoryBot.create(:post, user_id: user.id, category_id: category.id)
+    get posts_path
+    expect(response).to be_successful
+    expect(response.body).to include(post.title)
   end
 end
