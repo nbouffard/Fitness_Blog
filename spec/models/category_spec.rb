02527bd644630f92fe93bf35f85533ed47e94d50
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-11-16 11:33:17 +0100

    add category factory, refactored category spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it 'Should be blank' do
-    category = Category.new(category_name: '')
+  it 'Should not be blank' do
+    category = build(:category, category_name: '')
     expect(category).to_not be_valid
   end
 end
