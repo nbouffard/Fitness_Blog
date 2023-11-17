e4446ae68e9974f28f5ee3b805e4face1b603760
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-11-17 15:04:53 +0100

    remove reference

--- db/migrate/20231114074827_create_posts.rb
+++ db/migrate/20231114074827_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.text :content
       t.string :title
       t.references :user, null: false, foreign_key: true
-      t.references :category, null: false, foreign_key: true
+      # t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
