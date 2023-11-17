2ae8503d9de89af0f1bd7038db065b31e53f961d
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-11-17 15:07:10 +0100

    update post controller for post create, update new and show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,8 +11,33 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
-    # @tag = Tag.new
-    # @category = Category.new
-    # @user = current_user
+    @user = current_user
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    # @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post, status: :created
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :category)
   end
 end
+
+# if post_params[:tag_ids].present?
+#       post_params[:tag_ids].each do |tag_id|
+#         @post.post_tags.create(tag_id: tag_id)
+#       end
+#     end
+#     if post_params[:category_id].present?
+#       @post.category_id = post_params[:category_id]
+#     end
+# , tags_attributes: [:tags]
