660e4115ab99b65bdc0e4afe45f1db86092ba0d3
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-11-17 15:01:26 +0100

    add validation for category

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belongs_to :category
-  has_many :post_tags, dependent: :destroy
-  has_many :tags, through: :post_tags
+  # has_many :post_tags, dependent: :destroy
+  # has_many :tags, through: :post_tags
   has_many :comments, dependent: :destroy
+  # accepts_nested_attributes_for :tags, reject_if: :all_blank
 
+  CATEGORIES = ['one', 'two', 'three']
   validates :title, presence: true, length: { maximum: 60 }
-  validates :content, presence: true, length: { minimum: 1500 }
+  validates :content, presence: true, length: { minimum: 15 }
+  validates :category, presence: true
 end
