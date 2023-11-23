1c5afe9cc97c0140d699ba866e9f32067afb1e9e
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-11-23 10:49:04 +0100

    comment out unused tests

---
+++ app/policies/post_policy.rb
@@ -0,0 +1,32 @@
+class PostPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    user.present?
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
---
+++ spec/policies/post_policy_spec.rb
@@ -0,0 +1,28 @@
+# require 'rails_helper'
+# require 'pundit/rspec'
+
+# RSpec.describe PostPolicy, type: :policy do
+#   let(:user) { create(:user) }
+
+#   subject { described_class }
+
+#   permissions ".scope" do
+#     pending "add some examples to (or delete) #{__FILE__}"
+#   end
+
+#   permissions :show? do
+#     pending "add some examples to (or delete) #{__FILE__}"
+#   end
+
+#   permissions :create? do
+#     pending "add some examples to (or delete) #{__FILE__}"
+#   end
+
+#   permissions :update? do
+#     pending "add some examples to (or delete) #{__FILE__}"
+#   end
+
+#   permissions :destroy? do
+#     pending "add some examples to (or delete) #{__FILE__}"
+#   end
+# end
