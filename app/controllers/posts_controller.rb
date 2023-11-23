3892a91c8b2527db8d6ca95c4c2970666672c11e
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-11-23 10:48:30 +0100

    add pundit authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,20 +2,23 @@ class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
-    @posts = Post.all
+    @posts = policy_scope(Post)
   end
 
   def show
     @post = Post.find(params[:id])
+    authorize @post
   end
 
   def new
     @post = Post.new
+    authorize @post
     @user = current_user
   end
 
   def create
     @post = Post.new(post_params)
+    authorize @post
     @post.user = current_user
     # @post = current_user.posts.build(post_params)
     if @post.save
@@ -27,10 +30,12 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+    authorize @post
   end
 
   def update
     @post = Post.find(params[:id])
+    authorize @post
     if @post.user_id == current_user.id
       if @post.update(post_params)
         redirect_to post_path(@post), status: :accepted
@@ -42,6 +47,7 @@ class PostsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:id])
+    authorize @post
     @post.destroy
     redirect_to posts_path, status: :ok
   end
